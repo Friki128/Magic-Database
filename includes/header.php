57314d01fd9acbed300caccd84dc444d74c75447
Friki128 <82997689+Friki128@users.noreply.github.com>
2021-06-01 10:15:46 +0200

    Add files via upload

--- includes/header.php
+++ includes/header.php
@@ -2,4 +2,5 @@
     <a href="index.php">Pagina principal</a>
     <a href="insertar_carta.php">Insertar</a>
     <a href="taula.php">Taula</a>
+    <a href="Acerca.php">Acerca de la pagina</a>
 </header>
